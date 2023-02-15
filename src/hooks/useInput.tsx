db502ce5b223e59198050333f8cd171fa952b964
pg0817h <pg0817h@gmail.com>
2023-02-14 22:24:56 -0800

    fix(validate-error): fix the issue for error validation

--- src/hooks/useInput.tsx
+++ src/hooks/useInput.tsx
@@ -1,29 +1,31 @@
-import {InputProps} from "../types/InputProps";
-import React, {useCallback, useContext} from "react";
-import {FormContext} from "../components/SimpleForm";
+import { InputProps } from "../types/InputProps";
+import React, { useCallback, useContext } from "react";
+import { FormContext } from "../components/SimpleForm";
 
-interface UseInputProps extends Pick<InputProps, "source" | "validate"> {
-}
+interface UseInputProps extends Pick<InputProps, "source" | "validate"> {}
 
 function useInput(props: UseInputProps) {
-    const {setValues, values, setError, error} = useContext(FormContext);
-    const onChange = useCallback(
-        (v: string) => {
-            props.validate.forEach(validateFunc => {
-                const newError = validateFunc(v);
-                if (newError) {
-                    setError({...error, [props.source]: newError});
-                }
-            });
-            setValues({
-                ...values,
-                [props.source]: v,
-            });
-        },
-        [values]
-    );
+  const { setValues, values, setError, error } = useContext(FormContext);
+  const onChange = useCallback(
+    (v: string) => {
+      const errorArray: string[] = [];
+      props.validate.forEach((validateFunc) => {
+        const newError = validateFunc(v);
 
-    return {value: values[props.source], onChange, error};
+        if (newError) {
+          errorArray.push(newError);
+        }
+      });
+      setError({ ...error, [props.source]: errorArray });
+      setValues({
+        ...values,
+        [props.source]: v,
+      });
+    },
+    [values]
+  );
+
+  return { value: values[props.source], onChange, error };
 }
 
 export default useInput;
